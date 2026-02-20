076f1599c34e5ac6e8bd4a256c16a42504a9bf75
Serge Vakulenko <serge.vakulenko@gmail.com>
2026-02-20 12:16:07 -0800

    Add test msdos_v3_31.

--- tests/msdos_test.cpp
+++ tests/msdos_test.cpp
@@ -29,7 +29,6 @@ TEST_F(MachineTest, msdos_v1_12)
 TEST_F(MachineTest, msdos_v2_12)
 {
     machine.boot_disk(TEST_DIR "/../images/msdos2.12-360k.img");
-    //Machine::enable_trace("s");
     run("\r\r");
     EXPECT_EQ(cursor_row(), 12);
     EXPECT_EQ(cursor_col(), 2);
@@ -45,4 +44,24 @@ TEST_F(MachineTest, msdos_v2_12)
     EXPECT_EQ(get_line(10), "(C) Copyright Microsoft 1981, 82, 83");
 
     EXPECT_EQ(get_line(12), "A>");
+}
+
+//
+// Start MS-DOS version 3.31
+//
+TEST_F(MachineTest, msdos_v3_31)
+{
+    machine.boot_disk(TEST_DIR "/../images/msdos3.31-1.44m.img");
+    run("\r\r");
+    EXPECT_EQ(cursor_row(), 10);
+    EXPECT_EQ(cursor_col(), 2);
+    EXPECT_EQ(get_line(0), "Current date is Tue  1-01-1980");
+    EXPECT_EQ(get_line(1), "Enter new date (mm-dd-yy):");
+    EXPECT_EQ(get_line(2), "Current time is  0:00:00.00");
+    EXPECT_EQ(get_line(3), "Enter new time:");
+
+    EXPECT_EQ(get_line(6), "Microsoft(R) MS-DOS(R)  Version 3.31");
+    EXPECT_EQ(get_line(7), "             (C)Copyright Microsoft Corp 1981-1987");
+
+    EXPECT_EQ(get_line(10), "A>");
 }
